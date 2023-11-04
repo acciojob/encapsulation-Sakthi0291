145105837ac5b7809608a757f085ae45d23acc44
Sakthi0291 <sbb357753@gmail.com>
2023-11-04 16:52:29 +0530

    Code Changes 11

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
+
+    public static void Main(String args[])
+    {
+        RWOnly obj = new RWOnly();
+
+        obj.setName("Shakthi");
+
+        String name = obj.getName();
+        System.out.println(name);
+    }
   
 }
